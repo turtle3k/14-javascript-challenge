b7dd588114e17e4f31c9275d7a0498cbac82d90d
turtle3k <tim@thehertels.com>
2019-10-19 00:57:21 -0500

    makin updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,36 +5,42 @@ var tableData = data;
 // Select the button
 var button = d3.select("#filter-btn");
 
-button.on("click", function() {
+button.on("click", function () {
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    // Clear any previous table data
+    // tbody.innerHTML("");
+    // tbody.html("");
+    // $("#ufo-table").empty();
+    // document.getElementById("ufo-table").innerHTML = "";
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
 
-  console.log(inputValue);
-  console.log(tableData);
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    console.log(inputValue);
+    console.log(tableData);
 
-  // Console.log the filtered sitings data
-  console.log(filteredData);
-   
-  // Select table body list element in the table
-  var tbody = d3.select("tbody");
+    var sightingsData = tableData.filter(sighting => sighting.datetime === inputValue);
 
-  // Use d3 to update each cell's tex w/ sitings values
-  // ** put a forEach loop here to print write out each table entry
-  filteredData.forEach(function(sitingsReport) {  
-    var row = tbody.append("tr");
-    Object.defineProperties(sitingsReport).forEach(function([key, value]) {
-        console.log(key, value);
-        //Append a cell to the row for each value in the siting report object
-        var cell = row.append("td");
-        cell.text(value);
+    // Console.log the filtered sitings data
+    console.log(sightingsData);
+
+    // Select table body list element in the table
+    var tbody = d3.select("tbody");
+
+    // Use d3 to update each cell's text w/ sightings values
+    // ** put a forEach loop here to print write out each table entry
+    sightingsData.forEach(function (sightingsReport) {
+        var row = tbody.append("tr");
+        Object.entries(sightingsReport).forEach(function ([key, value]) {
+            console.log(key, value);
+            //Append a cell to the row for each value in the siting report object
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-  });
-  
+
 
 });
