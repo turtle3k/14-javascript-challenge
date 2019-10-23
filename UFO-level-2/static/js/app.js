d3f796153b26bbb06cf4191e395b3b410a8094d8
turtle3k <52467518+turtle3k@users.noreply.github.com>
2019-10-23 13:53:27 -0500

    updates to v2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -23,21 +23,39 @@ button.on("click", function () {
     let shapeInputValue = shapeInputElement.property("value").trim().toLowerCase();
 
     // Console.log the input filter selections (& tableData if needed for troubleshooting)
-    console.log(`DT: ${dtInputValue}, CI: ${cityInputValue},  ST: ${stateInputValue},  CO: ${countryInputValue}, SH: ${shapeInputValue}`);
+    console.log(`Date: ${dtInputValue}, City: ${cityInputValue},  State: ${stateInputValue},  Country: ${countryInputValue}, Shape: ${shapeInputValue}`);
     // console.log(tableData);
 
     // Filter the signtingsData based on the input filter(s)
-    var sightingsData = tableData.filter(sighting => sighting.datetime === dtInputValue);
+    //var sightingsData = tableData.filter(sighting => sighting.datetime === dtInputValue);
+    let sightingsData = []
 
-    
+    // Check if a filter Date was entered.  if not, return the whole dataset.
+    if (dtInputValue !== "") {
+        sightingsData = tableData.filter(sighting => sighting.datetime === dtInputValue);
+    } else {
+        sightingsData = tableData;
+    };
+    // Check if a City filter was entered.
+    if (cityInputValue !== "") {
+        sightingsData = sightingsData.filter(sighting => sighting.city === cityInputValue);
+    };
+    // Check if a State filter was entered.
+    if (stateInputValue !== "") {
+        sightingsData = sightingsData.filter(sighting => sighting.state === stateInputValue);
+    };
+    // Check if a Country filter was entered.
+    if (countryInputValue !== "") {
+        sightingsData = sightingsData.filter(sighting => sighting.country === countryInputValue);
+    };
+    // Check if a Shape filter was entered.
+    if (shapeInputValue !== "") {
+        sightingsData = sightingsData.filter(sighting => sighting.shape === shapeInputValue);
+    };
 
 
 
-
-
-
-
-    // Console.log the filtered sitings data
+    // Console.log the filtered sightings data
     console.log(sightingsData);
 
     // Select table body list element in the table
@@ -51,7 +69,7 @@ button.on("click", function () {
         var row = tbody.append("tr");
         Object.entries(sightingsReport).forEach(function ([key, value]) {
             console.log(key, value);
-            //Append a cell to the row for each value in the siting report object
+            //Append a cell to the row for each value in the sighting report object
             var cell = row.append("td");
             cell.text(value);
         });
